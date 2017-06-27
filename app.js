2c2dacc7fde2ce41b8ebc859d6d145e107a0a950
pennygibson <cenneboulet@yahoo.ca>
2017-06-27 17:35:58 +0000

    first commit of winnipeg diaper repo

---
+++ app.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+  $('#button1').click(function(){
+  $('#amp').show("slow");
+  })
+  
+  $('#button1Less').click(function(){
+      $('#amp').hide();
+  })
+  
+  $('#button2').click(function(){
+      $('#treehugger').show("slow");
+  })
+  $('#button2Less').click(function(){
+      $('#treehugger').hide();
+  })
+  
+  $('#button3').click(function(){
+      $('#bb').show("slow");
+  })
+  $('#button3Less').click(function(){
+      $('#bb').hide();
+  })
+  
+  $('#q1').click(function(){
+      $('#a1').show("slow");
+  })
+  $('#q2').click(function(){
+      $('#a2').show("slow");
+  })
+  $('#q3').click(function(){
+      $('#a3').show("slow");
+  })
+  $('#q4').click(function(){
+      $('#a4').show("slow");
+  })
+  $('#q5').click(function(){
+      $('#a5').show("slow");
+  })
+  $('#q6').click(function(){
+      $('#a6').show("slow");
+  })
+});
+
